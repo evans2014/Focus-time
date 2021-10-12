a2d55943a2a4b7efb7d0237421884bdabd4374d8
Ivan Vasilev <i.vass@abv.bg>
2021-10-12 16:18:46 +0300

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, AsyncStorage } from 'react-native';
 
-//import { uuidv4 } from './src/utils/uuid';
 import { Timer } from './src/features/timer/Timer';
 import { Focus } from './src/features/focus/Focus';
 import { FocusHistory } from './src/features/focus/FocusHistory';
